cd8746a1c33aeda99811157a635125ea2a7db6d2
ManasaBhimisetty792 <marymanasabhimisetty@gmail.com>
2026-07-08 16:46:32 +0530

    Clean up imports in views.py
    Removed unused imports related to Google authentication.

--- app1/views.py
+++ app1/views.py
@@ -16,8 +16,7 @@ import csv
 from django.conf import settings
 from django.contrib.auth.models import User
 from django.contrib.auth import login
-from google.oauth2 import id_token
-from google.auth.transport import requests
+
 from django.conf import settings as django_settings
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
@@ -140,7 +139,7 @@ def pricing(request):
 
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import login
+
 from .models import Profile
 
 def register_view(request):
